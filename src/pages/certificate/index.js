9799bf72eacb2f661ce5ce9e99af7ce5c6a0b30f
andikapriyotamad <andikapriyotamad@gmail.com>
2021-11-30 10:33:15 +0700

    Render secara kondisional data sertifikat yang terverifikasi

--- src/pages/certificate/index.js
+++ src/pages/certificate/index.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import styled from "styled-components";
 import { useParams, Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { getAuthenticationStore } from "store/slice/authentication";
@@ -10,6 +11,78 @@ import Footer from "layouts/landingpage/Footer";
 
 import logomyarchery from "assets/images/myachery/myachery.png";
 import { ReactComponent as IconCheck } from "assets/icons/check-circle.svg";
+
+const AlertVerifiedStyled = styled.div`
+  background-color: #efefef;
+  padding: 20px;
+`;
+
+function AlertVerified() {
+  return (
+    <AlertVerifiedStyled className="w-100 d-flex justify-content-center align-items-center rounded-3 mb-4">
+      <IconCheck stroke="#368759" className="me-2" />
+      <span>Sertifikat Terverifikasi</span>
+    </AlertVerifiedStyled>
+  );
+}
+
+function CardCertificateInfo({ data }) {
+  if (!data) {
+    return null;
+  }
+
+  return (
+    <Card className="text-muted">
+      <CardBody>
+        <Row className="mt-4">
+          <Col md="6">
+            <h6>Nama Archer</h6>
+            <p>Sudjiwo Tejo</p>
+          </Col>
+
+          <Col md="6">
+            <h6>Nomor ID</h6>
+            <p>X-XXX</p>
+          </Col>
+
+          <Col md="6">
+            <h6>Klub</h6>
+            <p>Memanah Club</p>
+          </Col>
+
+          <Col md="6">
+            <h6>Kategori</h6>
+            <p>Umum - Individu - Barebow</p>
+          </Col>
+
+          <Col md="6">
+            <h6>Peringkat</h6>
+            <p>Juara 1</p>
+          </Col>
+        </Row>
+
+        <hr className="mt-0" />
+
+        <Row className="mt-4">
+          <Col md="6">
+            <h6>Event</h6>
+            <p>The HuB Scoring 2021</p>
+          </Col>
+
+          <Col md="6">
+            <h6>Tanggal</h6>
+            <p>12 Oktober 2021</p>
+          </Col>
+
+          <Col md="6">
+            <h6>Lokasi</h6>
+            <p>The Hub, Bekasi</p>
+          </Col>
+        </Row>
+      </CardBody>
+    </Card>
+  );
+}
 
 export default function CertificateVerificationPage() {
   const { certificate_code } = useParams();
@@ -46,65 +119,8 @@ export default function CertificateVerificationPage() {
         </div>
 
         <Container className="mt-4 mb-5">
-          <div
-            className="d-flex justify-content-center align-items-center rounded-3 mb-4"
-            style={{ backgroundColor: "#EFEFEF", padding: 20 }}
-          >
-            <IconCheck stroke="#368759" className="me-2" />
-            <span>Sertifikat Terverifikasi</span>
-          </div>
-
-          <Card className="text-muted">
-            <CardBody>
-              <Row>
-                <Col md="6">
-                  <h6>Nama Archer</h6>
-                  <p>Sudjiwo Tejo</p>
-                </Col>
-
-                <Col md="6">
-                  <h6>Nomor ID</h6>
-                  <p>X-XXX</p>
-                </Col>
-
-                <Col md="6">
-                  <h6>Klub</h6>
-                  <p>Memanah Club</p>
-                </Col>
-
-                <Col md="6">
-                  <h6>Kategori</h6>
-                  <p>Umum - Individu - Barebow</p>
-                </Col>
-
-                <Col md="6">
-                  <h6>Peringkat</h6>
-                  <p>Juara 1</p>
-                </Col>
-              </Row>
-            </CardBody>
-          </Card>
-
-          <Card>
-            <CardBody>
-              <Row>
-                <Col md="6">
-                  <h6>Event</h6>
-                  <p>The HuB Scoring 2021</p>
-                </Col>
-
-                <Col md="6">
-                  <h6>Tanggal</h6>
-                  <p>12 Oktober 2021</p>
-                </Col>
-
-                <Col md="6">
-                  <h6>Lokasi</h6>
-                  <p>The Hub, Bekasi</p>
-                </Col>
-              </Row>
-            </CardBody>
-          </Card>
+          <AlertVerified />
+          <CardCertificateInfo data={null} />
         </Container>
 
         <div className="mt-auto position-relative" style={{ height: 83 }}>
