9f6f3e833c510041215d6cc1a63d3676a8a27c61
andikapriyotamad <andikapriyotamad@gmail.com>
2021-12-15 18:38:35 +0700

    Hapus routing admin gak terpakai

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,3 @@
-export { default as authenticationRoutes } from "./authentication";
-export { default as dashboardRoutes } from "./dashboard";
 export { default as workingRoutes } from "./working";
 export { default as eventRouters } from "./event";
 export { default as certificateRoutes } from "./certificate";
