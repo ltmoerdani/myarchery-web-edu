f7ea2d487e92d67999b8bd91f3bda15150128d38
putra32 <putralubis1432@gmail.com>
2022-01-20 11:13:52 +0700

    beautify format

--- src/routes/archer.js
+++ src/routes/archer.js
@@ -1,13 +1,15 @@
-import LoginArcher from "../pages/authArcher/login"
-import RegisterArcher from "../pages/authArcher/register"
-import Logout from "../pages/authArcher/logout"
-import ForgotPassword from "pages/authArcher/forgotPassword"
+import LoginArcher from "../pages/authArcher/login";
+import RegisterArcher from "../pages/authArcher/register";
+import Logout from "../pages/authArcher/logout";
+import ForgotPassword from "pages/authArcher/forgotPassword";
+import Verification from "pages/authArcher/verification";
 
 const archerRouters = [
-    { path: "/archer/login", component: LoginArcher, exact: true},
-    { path: "/archer/register", component: RegisterArcher, exact: true},
-    { path: "/archer/logout", component: Logout, exact: true},
-    { path: "/archer/forgot-password", component: ForgotPassword, exact: true},
-]
+  { path: "/archer/login", component: LoginArcher, exact: true },
+  { path: "/archer/register", component: RegisterArcher, exact: true },
+  { path: "/archer/logout", component: Logout, exact: true },
+  { path: "/archer/forgot-password", component: ForgotPassword, exact: true },
+  { path: "/archer/verification", component: Verification, exact: true },
+];
 
-export default archerRouters
+export default archerRouters;
