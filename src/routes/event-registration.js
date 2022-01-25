0f2ce68bffa9ef92ff89a535299f392a33b23eee
andikapriyotamad <andikapriyotamad@gmail.com>
2022-01-25 14:59:38 +0700

    Route URL khusus event registration

--- src/routes/event-registration.js
+++ src/routes/event-registration.js
@@ -1,5 +1,7 @@
 import PageEventRegistration from "pages/ma/event-registration";
 
-const eventRegistrationRoutes = [{ path: "/event/registration", component: PageEventRegistration }];
+const eventRegistrationRoutes = [
+  { path: "/event-registration/:slug", component: PageEventRegistration },
+];
 
 export default eventRegistrationRoutes;
