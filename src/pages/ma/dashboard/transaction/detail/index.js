d3732a7cad7d3029d61c6309d80f643bdc290131
putra32 <putralubis1432@gmail.com>
2022-01-27 09:59:38 +0700

    edit file to make detail transaction

--- src/pages/ma/dashboard/transaction/detail/index.js
+++ src/pages/ma/dashboard/transaction/detail/index.js
@@ -14,6 +14,9 @@ import {
 import { BreadcrumbDashboard } from "./components/breadcrumb";
 import MetaTags from "react-meta-tags";
 import classNames from "classnames";
+import { OrderEventService } from "services";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
 
 import event_img from "assets/images/myachery/a-1.jpg";
 
@@ -22,6 +25,9 @@ import Avatar from "./components/Avatar";
 
 function PageTransactionDetail() {
   const [activeTab, setActiveTab] = useState("1");
+  const [dataDetail, setDataDetail] = useState({});
+
+  const { orderId } = useParams();
 
   const breadcrumpCurrentPageLabel = "Kembali";
 
@@ -31,7 +37,34 @@ function PageTransactionDetail() {
     }
   };
 
-  useEffect(() => {
+  const statusPayment = (status) => {
+    if (status == 4) {
+      return "menunggu-pembayaran";
+    }
+    if (status == 3) {
+      return "gagal";
+    }
+    if (status == 2) {
+      return "kadarluarsa";
+    }
+    if (status == 1) {
+      return "di-ikuti";
+    }
+  };
+
+  console.log(statusPayment(3));
+
+  const getOrderEventBySlug = async () => {
+    const { data, message, errors } = await OrderEventService.get({ id: orderId });
+
+    if (data) {
+      setDataDetail(data);
+      console.log(message);
+    }
+    console.log(errors);
+  };
+
+  const paymentMidtrans = () => {
     const snapSrcUrl = "https://app.sandbox.midtrans.com/snap/snap.js";
     const myMidtransClientKey = "SB-Mid-client-y_BGhv-exWF6m27x"; //change this according to your client-key
 
@@ -45,11 +78,15 @@ function PageTransactionDetail() {
     return () => {
       document.body.removeChild(script);
     };
+  };
+
+  useEffect(() => {
+    getOrderEventBySlug();
+    paymentMidtrans();
   }, []);
 
   const handleClickPayment = () => {
-    console.log("klik bayar sekarang");
-    window.snap.pay("8a38cec9-eb94-470f-a189-2b4c1ed33261", {
+    window.snap.pay(dataDetail?.transactionInfo?.snapToken, {
       onSuccess: function () {
         console.log("success");
       },
@@ -63,6 +100,29 @@ function PageTransactionDetail() {
         console.log("customer closed the popup without finishing the payment");
       },
     });
+  };
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const dateEventStart = new Date(dataDetail?.archeryEvent?.eventStartDatetime);
+  const dateEventEnd = new Date(dataDetail?.archeryEvent?.eventEndDatetime);
+
+  const handlerEvenDate = (date) => {
+    const dateEvent = `${date?.getDate()} ${months[date?.getMonth()]} ${date?.getFullYear()}`;
+    return dateEvent;
   };
 
   return (
@@ -124,7 +184,7 @@ function PageTransactionDetail() {
                               height: "100%",
                               borderRadius: "10px",
                             }}
-                            src={event_img}
+                            src={dataDetail?.archeryEvent?.poster || event_img}
                           />
                         </div>
                       </Col>
@@ -133,25 +193,27 @@ function PageTransactionDetail() {
                           <tr>
                             <td>Nama Event</td>
                             <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-                            <td>: Pro Archery 2022</td>
+                            <td>: {dataDetail?.archeryEvent?.eventName}</td>
                             <hr />
                           </tr>
                           <tr>
                             <td>Jenis Event</td>
                             <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-                            <td>: DKI Jakarta Series</td>
+                            <td>: {dataDetail?.archeryEvent?.eventType}</td>
                             <hr />
                           </tr>
                           <tr>
                             <td>Lokasi</td>
                             <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-                            <td>: Jakarta</td>
+                            <td>: {dataDetail?.archeryEvent?.location}</td>
                             <hr />
                           </tr>
                           <tr>
                             <td>Tanggal</td>
                             <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-                            <td>: 01 Februari 2022 - 30 Maret 2022</td>
+                            <td>
+                              : {handlerEvenDate(dateEventStart)} - {handlerEvenDate(dateEventEnd)}
+                            </td>
                             <hr />
                           </tr>
                         </table>
@@ -169,37 +231,41 @@ function PageTransactionDetail() {
                       <tr>
                         <td>Nama Pendaftar</td>
                         <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-                        <td>: Muhammad Fauzi</td>
+                        <td>: {dataDetail?.participant?.name}</td>
                         <hr />
                       </tr>
                       <tr>
                         <td>Email</td>
                         <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-                        <td>: teronggoren@gmail.com</td>
+                        <td>: {dataDetail?.participant?.email}</td>
                         <hr />
                       </tr>
                       <tr>
                         <td>No. Telpon</td>
                         <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-                        <td>: 0812345678</td>
+                        <td>: {dataDetail?.participant?.phoneNumber}</td>
                         <hr />
                       </tr>
                     </table>
                     <div style={{ backgroundColor: "#E7EDF6" }}>
                       <p className="p-2 font-size-16">Peserta</p>
                     </div>
-                    <div className="d-flex">
-                      <div style={{ width: "100px" }}>
-                        <Avatar />
-                      </div>
-                      <div className="ms-4">
-                        <h5>Muhammad Fauzi</h5>
-                        <span>fauzi@gmail.com</span>
-                        <br />
-                        <span>Laki-laki</span>
-                        <span> 24 tahun</span>
-                      </div>
-                    </div>
+                    {dataDetail?.participant?.members.map((member) => {
+                      return (
+                        <div key={member?.id} className="d-flex">
+                          <div style={{ width: "100px" }}>
+                            <Avatar />
+                          </div>
+                          <div className="ms-4">
+                            <h5>{member?.name}</h5>
+                            <span>{member?.email}</span>
+                            <br />
+                            <span>{member?.gender === "male" ? "Laki-laki" : "Perempuan"}</span>
+                            <span> {member?.age} tahun</span>
+                          </div>
+                        </div>
+                      );
+                    })}
                   </div>
                 </CardBody>
               </Card>
@@ -212,18 +278,137 @@ function PageTransactionDetail() {
                       <tr>
                         <td>Jenis Regu</td>
                         <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-                        <td>: Individu Putra</td>
+                        <td>: {dataDetail?.participant?.teamCategoryId}</td>
                         <hr />
                       </tr>
                       <tr>
                         <td>Detal Katogri</td>
                         <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-                        <td>: Umum - Barebow - 50m</td>
+                        <td>
+                          : {dataDetail?.participant?.ageCategoryId} -{" "}
+                          {dataDetail?.participant?.competitionCategoryId} -{" "}
+                          {dataDetail?.participant?.distanceId}m
+                        </td>
                         <hr />
                       </tr>
                     </table>
+                    <hr />
                     <div>
-                      <div className="cut-text">hallo</div>
+                      <div>
+                        <div>
+                          <h5>Detail Pertandingan</h5>
+                          <div>
+                            <span className="p-1 btn mt-2" style={{ backgroundColor: "#EEE" }}>
+                              Selesai
+                            </span>
+                            <div className="mt-2">
+                              <h5>Gugur - Pro Archery 2022</h5>
+                              <table>
+                                <tr>
+                                  <td>Hari</td>
+                                  <td>Tanggal</td>
+                                  <td></td>
+                                  <td>Jam</td>
+                                  <td style={{ paddingLeft: "64px" }}>Bantalan</td>
+                                </tr>
+                                <tr>
+                                  <td>Sabtu</td>
+                                  <td>31 Desember 2022</td>
+                                  <td></td>
+                                  <td>08:00</td>
+                                  <td style={{ paddingLeft: "64px" }}>A1</td>
+                                </tr>
+                              </table>
+                            </div>
+                          </div>
+                          <div className="mt-4">
+                            <span
+                              className="p-1 btn mt-2"
+                              style={{ backgroundColor: "#E3DDF2", color: "#453473" }}
+                            >
+                              Kualifikasi
+                            </span>
+                            <div className="mt-2">
+                              <h5>Gugur - Pro Archery 2022</h5>
+                              <table>
+                                <tr>
+                                  <td>Hari</td>
+                                  <td>Tanggal</td>
+                                  <td></td>
+                                  <td>Jam</td>
+                                  <td style={{ paddingLeft: "64px" }}>Bantalan</td>
+                                </tr>
+                                <tr>
+                                  <td>Sabtu</td>
+                                  <td>31 Desember 2022</td>
+                                  <td></td>
+                                  <td>08:00</td>
+                                  <td style={{ paddingLeft: "64px" }}>A1</td>
+                                </tr>
+                              </table>
+                            </div>
+                          </div>
+                          <div className="mt-4">
+                            <span
+                              className="p-1 btn mt-2"
+                              style={{ backgroundColor: "#FFF8E9", color: "#FFB420" }}
+                            >
+                              Menungu Pembayaran
+                            </span>
+                            <div className="mt-2">
+                              <h5>Gugur - Pro Archery 2022</h5>
+                              <table>
+                                <tr>
+                                  <td>Hari</td>
+                                  <td>Tanggal</td>
+                                  <td></td>
+                                  <td>Jam</td>
+                                  <td style={{ paddingLeft: "64px" }}>Bantalan</td>
+                                </tr>
+                                <tr>
+                                  <td>Sabtu</td>
+                                  <td>31 Desember 2022</td>
+                                  <td></td>
+                                  <td>08:00</td>
+                                  <td style={{ paddingLeft: "64px" }}>-</td>
+                                </tr>
+                              </table>
+                            </div>
+                            <div className="w-50">
+                              <span className="ps-2">Biaya Pendaftaran</span>
+                              <span style={{ float: "right" }}>RP 205.000</span>
+                            </div>
+                          </div>
+                          <div className="mt-4">
+                            <span className="p-1 btn mt-2" style={{ backgroundColor: "#EEE" }}>
+                              Daftar
+                            </span>
+                            <div className="mt-2">
+                              <h5>Gugur - Pro Archery 2022</h5>
+                              <table>
+                                <tr>
+                                  <td>Hari</td>
+                                  <td>Tanggal</td>
+                                  <td></td>
+                                  <td>Jam</td>
+                                  <td style={{ paddingLeft: "64px" }}>Bantalan</td>
+                                </tr>
+                                <tr>
+                                  <td>Sabtu</td>
+                                  <td>31 Desember 2022</td>
+                                  <td></td>
+                                  <td>08:00</td>
+                                  <td style={{ paddingLeft: "64px" }}>-</td>
+                                </tr>
+                              </table>
+                              <div className="w-50">
+                                <span className="ps-2">Biaya Pendaftaran</span>
+                                <span style={{ float: "right" }}>RP 205.000</span>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </CardBody>
@@ -243,7 +428,7 @@ function PageTransactionDetail() {
                               height: "100%",
                               borderRadius: "10px",
                             }}
-                            src={event_img}
+                            src={dataDetail?.archeryEvent?.poster || event_img}
                           />
                         </div>
                       </Col>
@@ -251,26 +436,24 @@ function PageTransactionDetail() {
                         <table>
                           <tr>
                             <td>
-                              <h5>Pro Archery 2022</h5>
+                              <h5>{dataDetail?.archeryEvent?.eventName}</h5>
                             </td>
                           </tr>
                           <tr>
                             <td>
-                              <div className="mb-3">Lapangan Panahan Utama - Pro Archery</div>
+                              <div className="mb-3">{dataDetail?.archeryEvent?.location}</div>
                             </td>
                           </tr>
                           <tr>
                             <td>
-                              <span
-                                style={{
-                                  backgroundColor: "#FFF8E9",
-                                  color: "#FFB420",
-                                  borderRadius: "10px",
-                                }}
-                                className="py-2 px-2"
-                              >
-                                Menunggu Pembayaran
-                              </span>
+                              <WrapperPaymentStatus>
+                                <span
+                                style={{borderRadius: '10px', padding: '8px'}}
+                                  className={statusPayment(dataDetail?.transactionInfo?.statusId)}
+                                >
+                                  {dataDetail?.transactionInfo?.status}
+                                </span>
+                              </WrapperPaymentStatus>
                             </td>
                           </tr>
                         </table>
@@ -281,26 +464,30 @@ function PageTransactionDetail() {
                       <Col md={2}>
                         <span>Jenis Regu</span>
                         <div>
-                          <h5>Individu Putra</h5>
+                          <h5>{dataDetail?.participant?.teamCategoryId}</h5>
                         </div>
                       </Col>
                       <Col md={3}>
                         <span>Kategori</span>
                         <div>
-                          <h5>Umum - Barebow - 50m</h5>
+                          <h5>
+                            {dataDetail?.participant?.ageCategoryId} -{" "}
+                            {dataDetail?.participant?.competitionCategoryId} -{" "}
+                            {dataDetail?.participant?.distanceId}m
+                          </h5>
                         </div>
                       </Col>
                       <Col md={2}>
                         <span>Jumlah Peserta</span>
                         <div>
-                          <h5>1 Orang</h5>
+                          <h5>{dataDetail?.participant?.members.length} Orang</h5>
                         </div>
                       </Col>
                       <Col md={5}>
                         <div style={{ float: "right" }}>
                           <span>Biaya Pendaftaran</span>
                           <div>
-                            <h5>Rp238,000</h5>
+                            <h5>Rp.{dataDetail?.transactionInfo?.total}</h5>
                           </div>
                           <div>
                             <button
@@ -324,5 +511,27 @@ function PageTransactionDetail() {
     </React.Fragment>
   );
 }
+
+const WrapperPaymentStatus = styled.div`
+  .menunggu-pembayaran {
+    background-color: #fff8e9;
+    color: #ffb420;
+  }
+
+  .gagal {
+    background-color: #f1998e;
+    color: #e11900;
+  }
+
+  .di-ikuti {
+    background-color: #66d19e;
+    color: #03703c;
+  }
+
+  .kadarluarsa {
+    background-color: #afafaf;
+    color: #000;
+  }
+`;
 
 export default PageTransactionDetail;
