638d724cb34dafaeaf7b024d58a7f40e953df7db
putra32 <putralubis1432@gmail.com>
2022-01-30 11:11:53 +0700

    add breadcrumb-dashboard

--- src/pages/ma/dashboard/list-transaction/index.js
+++ src/pages/ma/dashboard/list-transaction/index.js
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from "react";
 import { Container } from "reactstrap";
 import CardTransaction from "./components/CardTransaction";
 import { OrderEventService } from "services";
+import { BreadcrumbDashboard } from "../components/breadcrumb";
 
 function ListTransactionPage() {
   const [dataEvent, setDataEvent] = useState();
+
+  const breadcrumpCurrentPageLabel = "Dashboard";
 
   const getAllOrderEvent = async () => {
     const { data, message, errors } = await OrderEventService.getAll();
@@ -22,6 +25,9 @@ function ListTransactionPage() {
   return (
     <React.Fragment>
       <Container fluid>
+      <BreadcrumbDashboard to="/dashboard">
+          {breadcrumpCurrentPageLabel}
+        </BreadcrumbDashboard>
         <div className="pt-5">
           {dataEvent?.map((event) => {
             return (
