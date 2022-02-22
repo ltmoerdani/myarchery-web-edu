4b0fddb10ebed773f1e206503bd0947eff424f5f
andikapriyotamad <andikapriyotamad@gmail.com>
2022-02-22 13:09:28 +0700

    Form pendaftaran tim tanpa nama tim & handle submit

--- src/components/ma/index.js
+++ src/components/ma/index.js
@@ -3,3 +3,5 @@ export * from "./wizard-view";
 export * from "./avatar-default";
 export * from "./avatar-club-default";
 export * from "./loading-spinner";
+export * from "./alert-submit-error";
+export * from "./alert-confirm-action";
