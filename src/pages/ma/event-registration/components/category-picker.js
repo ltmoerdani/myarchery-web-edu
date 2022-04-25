bfc72f06d799863e040393d5b5124f4d507db612
andikapriyotamad <andikapriyotamad@gmail.com>
2022-04-25 19:54:21 +0700

    Pengecekan gender user terhadap kategori

--- src/pages/ma/event-registration/components/category-picker.js
+++ src/pages/ma/event-registration/components/category-picker.js
@@ -115,16 +115,19 @@ function PickerControl({
   );
   const { userProfile } = useSelector(AuthStore.getAuthenticationStore);
 
-  const getGenderFromCategory = (teamCategoryId) => {
-    const isMaleCategory = ["individu male", "male_team", "mix_team"].some(
-      (team) => team === teamCategoryId
-    );
-    const isFemaleCategory = ["individu female", "female_team", "mix_team"].some(
-      (team) => team === teamCategoryId
-    );
+  const _checkIsGenderMatchesCategory = (teamCategoryId, userGender) => {
+    if (teamCategoryId === "mix_team") {
+      return true;
+    }
 
-    if (isMaleCategory) return "male";
-    if (isFemaleCategory) return "female";
+    const gendersByCategoryId = {
+      "individu male": "male",
+      male_team: "male",
+      "individu female": "female",
+      female_team: "female",
+    };
+
+    return gendersByCategoryId[teamCategoryId] === userGender;
   };
 
   const handleSelectCategory = (category) => onChange?.(category);
@@ -163,8 +166,10 @@ function PickerControl({
             {groupedCategories[selectedFilter].map((category) => {
               const isQuotaAvailable = Number(category.totalParticipant) < Number(category.quota);
               const shouldOptionDisabled = !isQuotaAvailable || !category.isOpen;
-              const categoryMatchesUser =
-                userProfile?.gender === getGenderFromCategory(category.teamCategoryId);
+              const categoryMatchesUser = _checkIsGenderMatchesCategory(
+                category.teamCategoryId,
+                userProfile?.gender
+              );
               return (
                 <CategoryItem key={category.id}>
                   <input
