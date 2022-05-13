b869959342f5980ed4dd30a735bf49b013eb58d0
andikapriyotamad <andikapriyotamad@gmail.com>
2022-05-13 15:14:09 +0700

    Event terakhir yang sedang berlangsung

--- src/pages/ma/events/index.js
+++ src/pages/ma/events/index.js
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom";
 import { useEventsList } from "./hooks/events-list";
 import styled from "styled-components";
 
-import { SpinnerDotBlock, ButtonOutlineBlue } from "components/ma";
+import Countdown from "react-countdown";
+import { SpinnerDotBlock, ButtonBlue, ButtonOutlineBlue } from "components/ma";
 
 import IconCalendar from "components/ma/icons/mono/calendar";
 import IconMapPin from "components/ma/icons/mono/map-pin";
@@ -21,11 +22,30 @@ function PageEventsList() {
     <PageWrapper>
       <InnerContentWrapper>
         <LatestEventWrapper>
-          <EventBanner>
+          <LatestEventBanner>
             {Boolean(events?.[0]) && (
               <img className="event-banner-image" src={events?.[0]?.poster} />
             )}
-          </EventBanner>
+          </LatestEventBanner>
+
+          <LatestEventCTABar>
+            <div>
+              <Countdown
+                date={datetime.parseServerDatetime(events?.[0]?.registrationEndDatetime)}
+                renderer={CustomCountDown}
+              />
+            </div>
+
+            <LatestEventCTACopywriting>
+              Segera daftarkan dirimu dan timmu pada kompetisi {events?.[0]?.eventName}
+            </LatestEventCTACopywriting>
+
+            <LatestEventCTAButtonsGroup>
+              <ButtonBlue block as={Link} to={_parseEventPath(events?.[0]?.eventUrl)}>
+                Daftar Sekarang
+              </ButtonBlue>
+            </LatestEventCTAButtonsGroup>
+          </LatestEventCTABar>
         </LatestEventWrapper>
 
         <ContentSectionBlock>
@@ -106,6 +126,40 @@ function PageEventsList() {
   );
 }
 
+function CustomCountDown({ days, hours, minutes, seconds, completed }) {
+  if (completed) {
+    return (
+      <div>
+        <span>Event Berakhir</span>
+      </div>
+    );
+  }
+
+  return (
+    <CountdownWrapper>
+      <CounterItem>
+        <CounterNumber>{days}</CounterNumber>
+        <CounterUnit>Hari</CounterUnit>
+      </CounterItem>
+
+      <CounterItem>
+        <CounterNumber>{hours}</CounterNumber>
+        <CounterUnit>Jam</CounterUnit>
+      </CounterItem>
+
+      <CounterItem>
+        <CounterNumber>{minutes}</CounterNumber>
+        <CounterUnit>Menit</CounterUnit>
+      </CounterItem>
+
+      <CounterItem>
+        <CounterNumber>{seconds}</CounterNumber>
+        <CounterUnit>Detik</CounterUnit>
+      </CounterItem>
+    </CountdownWrapper>
+  );
+}
+
 /* ============================= */
 // styles
 
@@ -120,7 +174,7 @@ const InnerContentWrapper = styled.div`
   margin: 0 auto;
   background-color: #ffffff;
 
-  @media (min-width: 769px) {
+  @media (min-width: 768px) {
     margin: 0 0.75rem;
   }
 
@@ -135,12 +189,12 @@ const ContentSectionBlock = styled.div`
 
 const LatestEventWrapper = styled.div`
   padding: 0;
-  @media (min-width: 769px) {
+  @media (min-width: 768px) {
     padding: 2rem;
   }
 `;
 
-const EventBanner = styled.div`
+const LatestEventBanner = styled.div`
   position: relative;
   width: 100%;
   padding-top: 42%;
@@ -155,6 +209,52 @@ const EventBanner = styled.div`
     object-fit: cover;
     width: 100%;
     height: 100%;
+  }
+`;
+
+const LatestEventCTABar = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  gap: 1.75rem;
+
+  margin-left: auto;
+  margin-right: auto;
+  padding: 2rem;
+  background-color: #ffffff;
+  box-shadow: 0px -8px 36px 0px rgba(0, 0, 0, 0.17);
+
+  transform: translateY(-2rem);
+
+  @media (min-width: 768px) {
+    max-width: 40rem;
+  }
+
+  @media (min-width: 1024px) {
+    flex-direction: row;
+    max-width: 73.375rem;
+  }
+`;
+
+const LatestEventCTACopywriting = styled.div`
+  max-width: 27.625rem;
+
+  @media (min-width: 768px) {
+    text-align: center;
+  }
+
+  @media (min-width: 1024px) {
+    text-align: left;
+  }
+`;
+
+const LatestEventCTAButtonsGroup = styled.div`
+  flex-shrink: 0;
+  width: 100%;
+
+  @media (min-width: 1024px) {
+    width: auto;
   }
 `;
 
@@ -184,7 +284,7 @@ const EventsGrid = styled.div`
   grid-template-columns: minmax(0, 1fr);
   gap: 1rem;
 
-  @media (min-width: 769px) {
+  @media (min-width: 768px) {
     grid-template-columns: repeat(auto-fill, minmax(360px, 1fr));
   }
 `;
@@ -286,6 +386,51 @@ const EventDescription = styled.p`
 const EventCardBottomAction = styled.div`
   display: flex;
   justify-content: flex-end;
+`;
+
+const CountdownWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 0.75rem;
+  text-align: center;
+`;
+
+const CounterItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0.5rem;
+  min-height: 6rem;
+  border-radius: 0.25rem;
+  border: solid 1px #eff2f7;
+
+  > *:first-child {
+    flex-grow: 1;
+  }
+
+  > *:last-child {
+    flex-shrink: 0;
+  }
+
+  @media (min-width: 520px) {
+    min-width: 6.245rem;
+  }
+`;
+
+const CounterNumber = styled.span`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  color: var(--ma-text-black);
+  font-size: 1.625rem;
+  font-weight: 600;
+`;
+
+const CounterUnit = styled.span`
+  padding: 0.25rem 0.5rem;
+  background-color: #eff2f7;
+  font-size: 0.75rem;
+  font-weight: 400;
 `;
 
 /* ============================== */
