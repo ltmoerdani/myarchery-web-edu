000c04f6b9a888bfd9c602807990329c67c49720
andikapriyotamad <andikapriyotamad@gmail.com>
2022-05-19 18:23:06 +0700

    Page reset password

--- src/routes/archer.js
+++ src/routes/archer.js
@@ -1,5 +1,4 @@
 import Logout from "../pages/authArcher/logout";
-import ForgotPassword from "pages/authArcher/forgotPassword";
 import Verification from "pages/authArcher/verification";
 import ResetPasswordArcher from "pages/authArcher/resetPassword";
 import ResetSuccessArcher from "pages/authArcher/resetSuccess";
@@ -7,12 +6,13 @@ import PolicyArcher from "pages/authArcher/policy";
 
 import PageAuthRegister from "pages/ma/auth-register";
 import PageAuthLogin from "pages/ma/auth-login";
+import PageAuthPasswordReset from "pages/ma/auth-password-reset";
 
 const archerRouters = [
   { path: "/archer/login", component: PageAuthLogin, exact: true },
   { path: "/archer/register", component: PageAuthRegister, exact: true },
   { path: "/archer/logout", component: Logout, exact: true },
-  { path: "/archer/forgot-password", component: ForgotPassword, exact: true },
+  { path: "/archer/forgot-password", component: PageAuthPasswordReset, exact: true },
   { path: "/archer/verification/:email", component: Verification, exact: true },
   { path: "/archer/reset-password/:email", component: ResetPasswordArcher, exact: true },
   { path: "/archer/reset-done/", component: ResetSuccessArcher, exact: true },
