3110091cadcba02e53c3d679c59a84a23db14def
cleoputra <acesam222@gmail.com>
2022-05-30 10:28:59 +0700

    meta for official

--- src/pages/ma/event-registrarion-official/index.js
+++ src/pages/ma/event-registrarion-official/index.js
@@ -201,14 +201,14 @@ function PageEventRegistration() {
     window.scrollTo(0, 0);
   }, [currentStep]);
 
-  console.log(eventDetail, 'detail');
-
   return (
     <StyledPageWrapper>
       <MetaTags>
         <title>
           Pendaftaran {eventDetailData?.publicInformation.eventName || ""} | MyArchery.id
         </title>
+        <meta id="meta-description" name="description" content="Bergabunglah bersama MyArchery.id" />
+        <meta id="og-title" property="og:title" content="MyArchery.id" />
       </MetaTags>
 
       <Container fluid>
